6a54646f723cd38fd26fe7ec819aa86e221ba48a
kashinath <kashinath.jadhav@successive.tech>
2023-08-29 17:29:51 +0530

    Folder structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Header of the file</h1>
+      <h1>Frontend Projectt</h1>
     </div>
   );
 }
